7e1890d970dc3fd41b44e105032980e0d85b5fc5
lvsl <lvsonglin@outlook.com>
2026-01-26 15:03:25 +0800

    delete

--- faceunity_ui_flutter/lib/faceunity_kit.dart
+++
@@ -1,7 +0,0 @@
-import 'package:faceunity_plugin/faceunity_plugin.dart';
-
-class FaceUnityKit {
-  static void setCameraPosition(bool front) async {
-    await FaceunityPlugin.setCameraPosition(front);
-  }
-}
